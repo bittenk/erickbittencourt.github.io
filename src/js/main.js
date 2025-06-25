2554c2401ed0e53f05e4922989a795034009a22f
ERICK BITTENCOURT DOS SANTOS <erickbittencourt1812@gmail.com>
2025-06-24 22:11:32 -0300

    finalizando projeto

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,31 @@
 const toggleTheme = document.getElementById("toggleTheme");
-const rootHtml = document.documentElement
+const rootHtml = document.documentElement;
+const accordionHeaders = document.querySelectorAll(".accordion__header");
+const menuLinks = document.querySelectorAll(".menu__link");
 
-function changetheme(){
-const currentTheme = rootHtml.getAttribute("data-theme");
-
-if(currentTheme === "dark") rootHtml.setAttribute("data-theme", "light")
-    else rootHtml.setAttribute("data-theme", "dark")
-
-    toggleTheme.classList.toggle("bi-sun")
-    toggleTheme.classList.toggle("bi-moon-stars")
-
-
+function changeTheme() {
+  const currentTheme = rootHtml.getAttribute("data-theme");
+  if (currentTheme === "dark") {
+    rootHtml.setAttribute("data-theme", "light");
+  } else {
+    rootHtml.setAttribute("data-theme", "dark");
+  }
+  toggleTheme.classList.toggle("bi-sun");
+  toggleTheme.classList.toggle("bi-moon-stars");
 }
 
-toggleTheme.addEventListener("click", changetheme);
+toggleTheme.addEventListener("click", changeTheme);
+
+accordionHeaders.forEach(header => {
+  header.addEventListener("click", () => {
+    const accordionItem = header.parentElement;
+    accordionItem.classList.toggle("active");
+  });
+});
+
+menuLinks.forEach(item => {
+  item.addEventListener("click", () => {
+    menuLinks.forEach(i => i.classList.remove("active"));
+    item.classList.add("active");
+  });
+});
